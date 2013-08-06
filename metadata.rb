08d7911d7166730d43af19a92ff88749f3bc3d1c
Dan Buch <d.buch@modcloth.com>
2013-08-06 13:22:08 -0400

    Getting vagrant testing up and s/Apache 2.0/MIT/

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'modcloth-stingray-exec'
 maintainer       'ModCloth, Inc.'
-maintainer_email 'external.tools+modcloth-stingray-cookbook@modcloth.com'
-license          'Apache v2.0'
+maintainer_email 'github+modcloth-stingray-exec-cookbook@modcloth.com'
+license          'MIT'
 description      'Installs/Configures modcloth-stingray'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
