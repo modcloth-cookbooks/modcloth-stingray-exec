de53cadec1fe9aa0dc74ae90d237a82c1943b131
Rafe Colton <r.colton@modcloth.com>
2013-08-13 10:33:56 -0400

    Bumping version one more

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'github+modcloth-stingray-exec-cookbook@modcloth.com'
 license          'MIT'
 description      'Installs/Configures modcloth-stingray'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
